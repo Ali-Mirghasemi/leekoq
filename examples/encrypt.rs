c7744768c41690a40475cdf9d98a49f5837fa2e5
Ali <ali.mirghasemi1376@gmail.com>
2023-01-14 20:25:25 +0330

    Add encrypt example

---
+++ examples/encrypt.rs
@@ -0,0 +1,24 @@
+use std::env;
+
+use leekoq::LeeKoq;
+
+fn parse(num: &str) -> u64 {
+    if num.starts_with("0x") {
+        u64::from_str_radix(&num[2..], 16).unwrap()
+    }
+    else {
+        u64::from_str_radix(num, 10).unwrap()
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        println!("Usage: cargo run --example encrypt -- <plain> <key>");
+    }
+    else {
+        let plain: u32 = parse(&args[1]) as u32;
+        let key: u64 = parse(&args[2]);
+        println!("cipher: {:08X}", LeeKoq::encrypt(plain, key));
+    }
+}
